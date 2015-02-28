7dc6c2810a1e7defb3857e589877334189c3fce2
Nick Ashley <nick@madlab.com>
2015-02-28 01:45:01 -0600

    Updating routes file for new routing core

--- routes.php
+++ routes.php
@@ -1,8 +1,14 @@
 <?php
 
 /**
- * phpCornerstone Custom Routes
+ * phpCornerstone Routes
  *
  */
 
+use MadLab\Cornerstone\Components\Config;
+use MadLab\Cornerstone\App;
+use MadLab\Cornerstone\Components\Routers\FileRouter;
+use MadLab\Cornerstone\Components\Routers\MapRouter;
 
+
+App::addRouter(new FileRouter(Config::get('NAKED_DOMAIN'), ''));
